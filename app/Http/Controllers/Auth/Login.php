9d8006298084235b8c58c8de20498e8dcefeb2f0
ZumaAkbarID <rahmatwahyumaakbar@gmail.com>
2023-01-30 03:25:42 +0700

    v1.1 login logger

--- app/Http/Controllers/Auth/Login.php
+++ app/Http/Controllers/Auth/Login.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Jobs\SendOtp;
+use App\Models\LoginLogger;
+use App\Models\WConfig;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,7 +14,7 @@ class Login extends Controller
     public function form()
     {
         return view('Auth.login', [
-            'title' => 'Daftar | ' . $this->WConfig['app_name'],
+            'title' => 'Masuk | ' . $this->WConfig['app_name'],
             'config' => $this->WConfig
         ]);
     }
@@ -37,6 +39,31 @@ class Login extends Controller
                     SendOtp::dispatch(Auth::user()->id, Auth::user()->phone_number);
                     return redirect()->intended(route('Auth_activation'));
                 } else {
+                    if (WConfig::where('key', 'app_env')->first()->value == 'production') {
+                        LoginLogger::create([
+                            'user_id'    => Auth::user()->id,
+                            'ip_address' => $request->getClientIp(),
+                            'user_agent' => $request->userAgent(),
+                        ]);
+                        send_dc_webhook('custom', [
+                            [
+                                'name' => 'User',
+                                'value' => Auth::user()->name,
+                                'inline' => false
+                            ],
+                            [
+                                'name' => 'IP',
+                                'value' => $request->getClientIp(),
+                                'inline' => false
+                            ],
+                            [
+                                'name' => 'User Agent',
+                                'value' => $request->userAgent(),
+                                'inline' => false
+                            ],
+                        ], null, 'https://discordapp.com/api/webhooks/1069350551034286120/5z9BKBMhNLsgZDZMUeXTCiFt1Sx0i2jtsCnhqWjUasPOFnt5NIvnQDxZtFyH8o6C73sn');
+                    }
+
                     return redirect()->intended('/dashboard');
                 }
             } else {
@@ -48,6 +75,31 @@ class Login extends Controller
                     SendOtp::dispatch(Auth::user()->id, Auth::user()->phone_number);
                     return redirect()->intended(route('Auth_activation'));
                 } else {
+                    if (WConfig::where('key', 'app_env')->first()->value == 'production') {
+                        LoginLogger::create([
+                            'user_id'    => Auth::user()->id,
+                            'ip_address' => $request->getClientIp(),
+                            'user_agent' => $request->userAgent(),
+                        ]);
+                        send_dc_webhook('custom', [
+                            [
+                                'name' => 'User',
+                                'value' => Auth::user()->name,
+                                'inline' => false
+                            ],
+                            [
+                                'name' => 'IP',
+                                'value' => $request->getClientIp(),
+                                'inline' => false
+                            ],
+                            [
+                                'name' => 'User Agent',
+                                'value' => $request->userAgent(),
+                                'inline' => false
+                            ],
+                        ], null, 'https://discordapp.com/api/webhooks/1069350551034286120/5z9BKBMhNLsgZDZMUeXTCiFt1Sx0i2jtsCnhqWjUasPOFnt5NIvnQDxZtFyH8o6C73sn');
+                    }
+
                     return redirect()->intended('/dashboard');
                 }
             } else {
---
+++ database/migrations/2023_01_29_200912_create_login_loggers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('login_loggers', function (Blueprint $table) {
+            $table->unsignedBigInteger('id', true);
+            $table->integer('user_id');
+            $table->string('ip_address');
+            $table->text('user_agent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('login_loggers');
+    }
+};
